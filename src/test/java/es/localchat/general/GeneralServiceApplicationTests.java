d85b8197f902dd6a70aab629942b53e9cff31840
sparkle <soroka.igor.olegovych@gmail.com>
2024-11-30 11:02:53 +0200

    init

---
+++ src/test/java/es/localchat/general/GeneralServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.localchat.general;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
